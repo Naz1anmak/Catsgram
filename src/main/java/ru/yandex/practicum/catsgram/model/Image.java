926813a2345c0c5f8958694adf8ff2b382d4e4ad
Alexander <fign.9polna@yandex.ru>
2025-03-05 21:25:56 +0300

    fix codestyle

--- src/main/java/ru/yandex/practicum/catsgram/model/Image.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Image.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.catsgram.model;
 
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
